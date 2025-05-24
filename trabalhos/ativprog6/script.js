6e786f9d9f6a4f3ef1af48fba864005c5121e27a
Jucélio José <juceliojdc@gmail.com>
2025-05-24 10:56:23 -0300

    refatoração para uma única função

--- trabalhos/ativprog6/script.js
+++ trabalhos/ativprog6/script.js
@@ -6,66 +6,41 @@ function lidarComEnvio() {
     const form = document.querySelector("form");
     const inputs = document.querySelectorAll("#usuario, #senha, #nome, #email, #cpf, #idade, #tel, #cep, #tipo-logradouro, #logradouro, #bairro, #cidade, #estado");
 
-    form.addEventListener("submit", (enviar) => {
+    form.addEventListener("submit", (envio) => {
         inputs.forEach((input) => {
 
-            const label = input.previousElementSibling;
+            valorVazio = input.value.trim() === "";
+            inserirSpan(input, valorVazio, envio, "<span>requerido</span>")
 
-            spanValidarCampoVazio(label, input, enviar);
-            
-            spanValidarUsuario(label, input, enviar);
-            
-            spanValidarSenha(label, input, enviar);
-            
-            spanValidarEmail(label, input, enviar);
+            if(input.getAttribute("id") === "usuario"){
+                menorQue5 = input.value.length <= 5;
+                inserirSpan(input, menorQue5, envio, "<span>requer mais de 5 caracteres</span>");
+            }
+
+            if(input.getAttribute("id") === "senha"){
+                const senhaValida = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9\s]).{9,}$/;
+                senhaInvalida = !senhaValida.test(input.value)
+                inserirSpan(input, senhaInvalida, envio, "<span> requer mais de 8 caracteres contendo: números, letras e símbolos</span>");
+            }
+
+            if(input.getAttribute("id") === "email"){
+                const emailValido = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                emailInvalido = !emailValido.test(input.value);
+                inserirSpan(input, emailInvalido, envio, "<span>requer um email válido</span>");
+            }
         })
     });
 }
 
-function removerSpansFilhos(elemento) {
-    elemento.querySelectorAll('span').forEach(span => {
+function inserirSpan(input, condicao, envio, msgSpan){
+    const elementoAnterior = input.previousElementSibling;
+
+    elementoAnterior.querySelectorAll('span').forEach(span => {
         span.remove()
     });
-}
 
-function spanValidarCampoVazio(elementoPai, input, enviar) {
-    removerSpansFilhos(elementoPai)
-    if (input.value.trim() === "") {
-        elementoPai.innerHTML += "<span>requerido</span>";
-        enviar.preventDefault();
-    } 
-}
-
-function spanValidarUsuario(elementoPai, input, enviar) {
-    if (input.getAttribute("id") === "usuario") {
-        if (input.value.length <= 5) {
-            removerSpansFilhos(elementoPai)
-            elementoPai.innerHTML += "<span>requer mais de 5 caracteres</span>";
-            enviar.preventDefault();
-        }
-    }
-}
-
-function spanValidarSenha(elementoPai, input, enviar) {
-    const senhaValida = /^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9\s]).{9,}$/;
-
-    if (input.getAttribute("id") === "senha") {
-        if (!senhaValida.test(input.value)) {
-            removerSpansFilhos(elementoPai)
-            elementoPai.innerHTML += "<span> requer mais de 8 caracteres contendo: números, letras e símbolos</span>";
-            enviar.preventDefault();
-        }
-    }
-}
-
-function spanValidarEmail(elementoPai, input, enviar){
-    const emailValido = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-    if (input.getAttribute("id") === "email") {
-        if (!emailValido.test(input.value)) {
-            removerSpansFilhos(elementoPai)
-            elementoPai.innerHTML += "<span>requer um email válido</span>";
-            enviar.preventDefault();
-        } 
+    if (condicao) {
+        elementoAnterior.innerHTML += msgSpan;
+        envio.preventDefault();
     }
 }
